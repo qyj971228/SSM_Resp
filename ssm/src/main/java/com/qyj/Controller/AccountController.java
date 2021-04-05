79c658204f290abb7065710c12d1058f78ae510d
qyj <1078869122@qq.com>
2021-04-05 17:59:48 +0800

    demo!succeed!2021年4月5日17:59:42

--- ssm/src/main/java/com/qyj/Controller/AccountController.java
+++ ssm/src/main/java/com/qyj/Controller/AccountController.java
@@ -1,13 +1,17 @@
 package com.qyj.Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qyj.domain.Account;
 import com.qyj.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -18,20 +22,29 @@ public class AccountController {
     private AccountService accountService;
 
     //保存
-    @RequestMapping(value = "/save",produces = "text/html;charset=UTF-8")
+    @RequestMapping(value = "/save", produces = "text/html;charset=UTF-8")
     @ResponseBody
-    public String save(Account account){
+    public String save(Account account) {
         accountService.save(account);
         return "保存成功";
     }
 
     //查询
     @RequestMapping("/findAll")
-    public ModelAndView findAll(){
+    public ModelAndView findAll() {
         List<Account> accountList = accountService.findAll();
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("accountList",accountList);
+        modelAndView.addObject("accountList", accountList);
         modelAndView.setViewName("accountList");
         return modelAndView;
+    }
+
+    //返回json
+    @RequestMapping("/Json")
+    @ResponseBody
+    @CrossOrigin(origins = "*")
+    public List<Account> save10(){
+        List<Account> accountList = accountService.findAll();
+        return accountList;
     }
 }
